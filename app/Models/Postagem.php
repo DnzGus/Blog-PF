665eb7e82249700ac71c85d3101688cbd122aa3e
DnzGus <gustavodtsilva@gmail.com>
2024-09-10 21:28:53 -0300

    log audit

--- app/Models/Postagem.php
+++ app/Models/Postagem.php
@@ -4,10 +4,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Postagem extends Model
+class Postagem extends Model implements Auditable
 {
     use HasFactory;
+    use \OwenIt\Auditing\Auditable;
 
     protected $table = 'postagens';
 
